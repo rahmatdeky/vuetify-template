a2dd6ffd5a3c5121ecb96b3b2992d6dcb6ecec26
Muhamad Husni <unny.husni@gmail.com>
2021-12-29 18:08:24 +0700

    Progress Barang Pindahan

--- src/views/barang-pindahan/Perekaman.vue
+++ src/views/barang-pindahan/Perekaman.vue
@@ -50,7 +50,6 @@
                     :rules="[rules.required]"
                     v-model="alamat_pemohon"
                     flat
-                    outlined
                   ></v-textarea>
                   <v-switch
                     v-model="fl_ppjk"
@@ -120,11 +119,120 @@
           </v-stepper-content>
 
           <v-stepper-content step="2">
-            <v-card
-              class="mb-12"
-              color="grey lighten-1"
-              height="200px"
-            ></v-card>
+            <v-form ref="form2">
+            <v-row>
+              <v-col cols="12" md="4">
+                <v-text-field
+                  type="number"
+                  label="Jumlah Kemasan"
+                  v-model="jumlah_kemasan"
+                  :rules="[rules.required, rules.kemasan]"
+                  required
+                >
+                </v-text-field>
+              </v-col>
+              <v-col cols="12" md="4">
+                <v-autocomplete
+                  label="Jenis Kemasan"
+                  v-model="jenis_kemasan"
+                  :items="kemasan_items"
+                  :item-text="(item) => `${item.id_kemasan} - ${item.kemasan}`"
+                  item-value="id_kemasan"
+                  :rules="[rules.required]"
+                ></v-autocomplete>
+              </v-col>
+              <v-col cols="12" md="4">
+                <v-text-field
+                  type="number"
+                  label="Berat (KG)"
+                  v-model="berat"
+                  :rules="[rules.required]"
+                  required
+                >
+                </v-text-field>
+              </v-col>
+            </v-row>
+            </v-form>
+            <div class="py-3">
+              <v-dialog v-model="dialog" width="500">
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn color="primary" v-bind="attrs" v-on="on">
+                    <v-icon>mdi-plus</v-icon> Tambah Barang
+                  </v-btn>
+                </template>
+
+                <v-card>
+                  <v-form ref="formBarang">
+                    <v-card-title class="text-h5 text--white">
+                      Tambah barang
+                    </v-card-title>
+
+                    <v-card-text>
+                      <v-text-field
+                        label="Uraian Barang"
+                        v-model="nama_barang"
+                        :rules="[rules.required]"
+                      >
+                      </v-text-field>
+                      <v-text-field
+                        label="Jumlah Barang"
+                        v-model="jumlah_barang"
+                        type="number"
+                        :rules="[rules.required]"
+                      >
+                      </v-text-field>
+                      <v-text-field
+                        label="Satuan Barang"
+                        v-model="satuan_barang"
+                        :rules="[rules.required]"
+                      >
+                      </v-text-field>
+                      <v-text-field
+                        label="Nilai Barang"
+                        type="number"
+                        v-model="nilai_barang"
+                        :rules="[rules.required]"
+                      >
+                      </v-text-field>
+                    </v-card-text>
+
+                    <v-divider></v-divider>
+
+                    <v-card-actions>
+                      <v-spacer></v-spacer>
+                      <v-btn color="primary" text @click="tambahBarang">
+                        <v-icon>mdi-plus</v-icon> Tambah
+                      </v-btn>
+                    </v-card-actions>
+                  </v-form>
+                </v-card>
+              </v-dialog>
+            </div>
+            <v-spacer></v-spacer>
+            <v-simple-table>
+              <template v-slot:default>
+                <thead>
+                  <tr>
+                    <th class="text-left">Nama Barang</th>
+                    <th class="text-left">Jumlah Barang</th>
+                    <th class="text-left">Nilai Barang</th>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="item in barang" :key="item.nama_barang">
+                    <td>{{ item.nama_barang }}</td>
+                    <td>{{ `${item.jumlah_barang} ${item.satuan_barang}` }}</td>
+                    <td>{{ item.nilai_barang }}</td>
+                    <td class="text-xs-left">
+                      <v-btn icon @click="removeBarang(item)">
+                        <v-icon>mdi-delete</v-icon>
+                      </v-btn>
+                    </td>
+                  </tr>
+                </tbody>
+              </template>
+            </v-simple-table>
           </v-stepper-content>
 
           <v-stepper-content step="3">
@@ -145,7 +253,7 @@
         </v-btn>
       </div>
       <div>
-        <v-btn text @click="next()" v-if="e1 < 3">
+        <v-btn text @click="next()" v-if="e1 < 4">
           Next
           <v-icon right dark>mdi-arrow-right</v-icon>
         </v-btn>
@@ -161,16 +269,21 @@ export default {
       e1: 1,
       isEditing: true,
       valid: false,
+      dialog: false,
       nama_pemohon: "",
       identitas_pemohon: "",
       alamat_pemohon: "",
+      fl_ppjk: false,
       nama_ppjk: "",
       identitas_ppjk: "",
       alamat_ppjk: "",
       moda_angkutan: "",
       pelabuhan_muat: "",
       pelabuhan_bongkar: "",
-      fl_ppjk: false,
+      jumlah_kemasan: "",
+      jenis_kemasan: "",
+      berat: "",
+      barang: [],
       rules: {
         required: (value) => !!value || "Required.",
         ktp: (value) => value.length === 16 || "Nomor KTP harus 16 digit",
@@ -183,8 +296,16 @@ export default {
         pelabuhan: () =>
           this.pelabuhan_muat !== this.pelabuhan_bongkar ||
           "Pelabuhan muat dan bongkar tidak boleh sama",
+        kemasan: (v) =>
+          v == this.barang.length ||
+          "Jumlah Kemasan tidak sama dengan Data Barang",
       },
       email: "",
+      // isian modal
+      nama_barang: "",
+      jumlah_barang: "",
+      satuan_barang: "",
+      nilai_barang: "",
       //referensi
       moda_angkutan_items: [
         { id: 1, moda: "Angkutan Laut" },
@@ -194,17 +315,43 @@ export default {
         { id_pelabuhan: "IDBTH", nama_pelabuhan: "Hang Nadim" },
         { id_pelabuhan: "IDBTU", nama_pelabuhan: "Batu Ampar" },
       ],
+      kemasan_items: [{ id_kemasan: "PK", kemasan: "Package" }],
     };
   },
   methods: {
     next() {
-      if (this.$refs.form.validate()) {
+      var form = null;
+      if (this.e1 == 1) {
+        form = "form";
+      } else if (this.e1 == 2) {
+        form = "form2";
+      } else if (this.e1 == 3) {
+        this.e1++;
+        return;
+      }
+      if (this.$refs.[form].validate()) {
         this.e1++;
       }
     },
     back() {
       this.e1--;
     },
+    tambahBarang() {
+      this.barang.push({
+        nama_barang: this.nama_barang,
+        jumlah_barang: this.jumlah_barang,
+        satuan_barang: this.satuan_barang,
+        nilai_barang: this.nilai_barang,
+      });
+      this.$refs.formBarang.reset();
+      this.dialog = false;
+    },
+    removeBarang(id) {
+      this.barang.splice(id, 1);
+    },
+  },
+  computed: {
+    itemsKemasan: (items) => `${items.id_kemasan} - ${items.kemasan}`,
   },
   beforeRouteLeave(to, from, next) {
     if (this.isEditing) {
