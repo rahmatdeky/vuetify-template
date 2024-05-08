6f8c1c00b30a0499c087f1058f36d7a0cca642b9
bcbatam <kpubcbatam@kemenkeu.go.id>
2024-05-08 19:52:07 +0700

    tampilan data untuk referensi kegiatan, kec, kel

--- src/views/settings/referensi/refKecamatan.vue
+++ src/views/settings/referensi/refKecamatan.vue
@@ -5,6 +5,45 @@
                 <v-app-bar-nav-icon></v-app-bar-nav-icon>
                 Referensi Kecamatan
             </v-toolbar>
+            <v-container>
+                <v-row>
+                    <v-col cols="3">
+                        <v-text-field label="Pencarian"></v-text-field>
+                    </v-col>
+                    <v-col align-self="center" cols="1">
+                        <v-btn icon>
+                            <v-icon>mdi-magnify</v-icon>
+                        </v-btn>
+                    </v-col>
+                    <v-col>
+                        <v-btn class="float-end" color="primary">
+                            <v-icon>mdi-plus</v-icon> Tambah
+                        </v-btn>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-simple-table>
+                        <thead>
+                            <tr>
+                                <th>Id Kecamatan</th>
+                                <th>Nama Kecamatan</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Sekupang</td>
+                                <td>
+                                    <v-btn class="float-end" color="warning" outlined>
+                                        <v-icon>mdi-pencil</v-icon>
+                                    </v-btn>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </v-simple-table>
+                </v-row>
+            </v-container>
         </v-card>
     </div>
 </template>
