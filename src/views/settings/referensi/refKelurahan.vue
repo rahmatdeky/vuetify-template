23ef1a244385d505c39fd16e20f9aab43832c5b6
bcbatam <kpubcbatam@kemenkeu.go.id>
2024-05-13 17:39:42 +0700

    validasi tambah kelurahan harus diisi get data kecamatan untuk dropdown tambah dan edit kelurahan fungsionalitas tambah kelurahan browse kelurahan, list kelurahan, dan pencarian kelurahan pengaturan modal edit kelurahan fungsionalitas edit kelurahan

--- src/views/settings/referensi/refKelurahan.vue
+++ src/views/settings/referensi/refKelurahan.vue
@@ -8,7 +8,7 @@
             <v-container>
                 <v-row>
                     <v-col cols="3">
-                        <v-text-field label="Pencarian"></v-text-field>
+                        <v-text-field v-model="search" label="Pencarian"></v-text-field>
                     </v-col>
                     <v-col align-self="center" cols="1">
                         <v-btn icon>
@@ -27,15 +27,17 @@
                             <tr>
                                 <th>Id Kelurahan</th>
                                 <th>Nama Kelurahan</th>
+                                <th>Nama Kecamatan</th>
                                 <th></th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>1</td>
-                                <td>Tiban</td>
+                            <tr v-for="data in filteredItems" :key="data.kode_kelurahan">
+                                <td>{{ data.kode_kelurahan }}</td>
+                                <td>{{ data.nama_kelurahan }}</td>
+                                <td>{{ data.kecamatan.nama_kecamatan }}</td>
                                 <td>
-                                    <v-btn @click="openModalEditKelurahan()" class="float-end" color="warning" outlined>
+                                    <v-btn @click="openModalEditKelurahan(data.kode_kelurahan)" class="float-end" color="warning" outlined>
                                         <v-icon>mdi-pencil</v-icon>
                                     </v-btn>
                                 </td>
@@ -49,20 +51,20 @@
             <v-card>
                 <v-toolbar color="primary" dark>Tambah Kelurahan</v-toolbar>
                 <v-container>
-                    <v-form>
+                    <v-form ref="formAddKelurahan" @submit.prevent="saveNewKelurahan()">
                         <v-row>
                             <v-col>
-                                <v-select label="Kecamatan" :items="listKecamatan" item-text="Kecamatan" item-value="id"></v-select>
+                                <v-select v-model="newKelurahan.kodeKecamatan" :rules="[rules.required]" label="Kecamatan" :items="listKecamatan" item-text="nama_kecamatan" item-value="kode_kecamatan"></v-select>
                             </v-col>
                         </v-row>
                         <v-row>
                             <v-col>
-                                <v-text-field label="Nama Kelurahan"></v-text-field>
+                                <v-text-field v-model="newKelurahan.namaKelurahan" :rules="[rules.required]" label="Nama Kelurahan"></v-text-field>
                             </v-col>
                         </v-row>
                         <v-row>
                             <v-col>
-                                <v-btn class="float-end" color="primary" dark>Simpan</v-btn>
+                                <v-btn type="submit" class="float-end" color="primary" dark>Simpan</v-btn>
                             </v-col>
                         </v-row>
                     </v-form>
@@ -73,20 +75,20 @@
             <v-card>
                 <v-toolbar color="primary" dark>Edit Kelurahan</v-toolbar>
                 <v-container>
-                    <v-form>
+                    <v-form ref="formEditKelurahan" @submit.prevent="saveEditKelurahan()">
                         <v-row>
                             <v-col>
-                                <v-select label="Kecamatan" :items="listKecamatan" item-text="Kecamatan" item-value="id"></v-select>
+                                <v-select v-model="selectedKelurahan.kecamatan" label="Kecamatan" :items="listKecamatan" item-text="nama_kecamatan" item-value="kode_kecamatan" :rules="[rules.required]"></v-select>
                             </v-col>
                         </v-row>
                         <v-row>
                             <v-col>
-                                <v-text-field label="Nama Kelurahan"></v-text-field>
+                                <v-text-field v-model="selectedKelurahan.nama_kelurahan" label="Nama Kelurahan" :rules="[rules.required]"></v-text-field>
                             </v-col>
                         </v-row>
                         <v-row>
                             <v-col>
-                                <v-btn class="float-end" color="primary" dark>Simpan</v-btn>
+                                <v-btn type="submit" class="float-end" color="primary" dark>Simpan</v-btn>
                             </v-col>
                         </v-row>
                     </v-form>
@@ -96,22 +98,113 @@
     </div>
 </template>
 <script>
+import Swal from 'sweetalert2'
 export default {
     data() {
         return {
             modalAddKelurahan: false,
             modalEditKelurahan: false,
-            listKecamatan: [
-                { id: 1, Kecamatan: 'Sekupang' },
-            ]
+            listKecamatan: [],
+            rules: {
+                required: value => !!value || 'Required.'
+            },
+            newKelurahan: {
+                kodeKecamatan: '',
+                namaKelurahan: ''
+            },
+            dataKelurahan: [],
+            search: '',
+            selectedKelurahan: {}
         }
     },
     methods: {
         openModalAddKelurahan() {
             this.modalAddKelurahan = true
         },
-        openModalEditKelurahan() {
+        openModalEditKelurahan(id) {
             this.modalEditKelurahan = true
+            this.selectedKelurahan = this.dataKelurahan.find((data) => data.kode_kelurahan === id)
+        },
+        saveNewKelurahan() {
+            if (this.$refs.formAddKelurahan.validate()) {
+                Swal.fire({
+                    title: 'Apa Anda Yakin?',
+                    text: "Anda Akan Menyimpan ini",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Yes'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        this.$http.post('ref/kelurahan/add', this.newKelurahan)
+                            .then((response) => {
+                                Swal.fire({
+                                    icon: response.data.icon,
+                                    title: response.data.title,
+                                    text: response.data.text
+                                }).then(() => {
+                                    this.modalAddKelurahan = false
+                                    this.getDataKelurahan()
+                                })
+                            })
+                    }
+                })
+            }
+        },
+        getDataKecamatan() {
+            this.$http.get('ref/kecamatan/browse').then((response) => {
+                this.listKecamatan = response.data
+            })
+        },
+        getDataKelurahan() {
+            this.$http.get('ref/kelurahan/browse').then((response) => {
+                this.dataKelurahan = response.data
+            })
+        },
+        saveEditKelurahan() {
+            if (this.$refs.formEditKelurahan.validate()) {
+                Swal.fire({
+                    title: 'Apa Anda Yakin?',
+                    text: "Anda Akan Menyimpan ini",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Yes'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        var data = {
+                            id: this.selectedKelurahan.kode_kelurahan,
+                            namaKelurahan: this.selectedKelurahan.nama_kelurahan,
+                            kodeKecamatan: this.selectedKelurahan.kecamatan
+                        }
+                        this.$http.post('ref/kelurahan/edit', data)
+                            .then((response) => {
+                                Swal.fire({
+                                    icon: response.data.icon,
+                                    title: response.data.title,
+                                    text: response.data.text
+                                }).then(() => {
+                                    this.modalEditKelurahan = false
+                                    this.getDataKelurahan()
+                                })
+                            })
+                    }
+                })
+            }
+        }
+    },
+    mounted() {
+        this.getDataKecamatan()
+        this.getDataKelurahan()
+    },
+    computed: {
+        filteredItems() {
+            return this.dataKelurahan.filter(item => 
+                item.nama_kelurahan.toLowerCase().includes(this.search.toLowerCase()) ||
+                item.kecamatan.nama_kecamatan.toLowerCase().includes(this.search.toLowerCase())
+            )
         }
     }
 }
